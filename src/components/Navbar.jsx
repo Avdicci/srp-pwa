6ac000402ad1b53e5852b4d2d530b63a680695b2
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-05-06 15:03:47 +0200

    002-Navbar: Create a navbar and style it to perfection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar() {
 const pathname = usePathname();
 
 return (
-<nav className="bg-gray-900 text-white px-6 py-4 shadow-md">
-    <div className="max-w-4xl mx-auto flex items-center justify-between">
-    <div className="flex items-center gap-6 text-sm font-medium">
+<nav className="bg-gradient-to-r from-indigo-500 to-blue-700 text-white px-6 py-6 shadow-md">
+    <div className="max-w-4xl mx-auto flex items-center justify-center">
+    <div className="flex items-center gap-6 text-md font-medium">
         <NavLink href="/" label="Home" active={pathname === '/'} />
         <NavLink href="/history" label="History" active={pathname === '/history'} />
     </div>
@@ -21,9 +21,9 @@ return (
 function NavLink({ href, label, active }) {
     return (
     <Link
-        href={href}
-        className={`transition-colors hover:text-blue-400 ${
-        active ? 'text-blue-400 underline underline-offset-4' : ''
+    href={href}
+    className={`transition-colors hover:text-violet-500 ${
+    active ? 'text-blue-300 underline underline-offset-4' : ''
         }`}
     >
         {label}
