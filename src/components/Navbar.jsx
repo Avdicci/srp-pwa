a31bdd40df6aebfc3661364f42bd0f4e478dbcc0
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-05-11 20:18:18 +0200

    023: Adding settings page for dev only

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,29 +4,31 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
-const pathname = usePathname();
+    const pathname = usePathname();
 
-return (
-<nav className="bg-gradient-to-r from-indigo-500 to-blue-700 text-white px-6 py-7 shadow-md">
-    <div className="max-w-4xl mx-auto flex items-center justify-center">
-    <div className="flex items-center gap-6 text-md font-medium">
-        <NavLink href="/" label="Home" active={pathname === '/'} />
-        <NavLink href="/history" label="History" active={pathname === '/history'} />
-    </div>
-    </div>
-</nav>
-);
+    return (
+        <nav className="bg-gradient-to-r from-indigo-500 to-blue-700 text-white px-6 py-7 shadow-md">
+            <div className="max-w-4xl mx-auto flex items-center justify-center">
+                <div className="flex items-center gap-6 text-md font-medium">
+                    <NavLink href="/" label="Home" active={pathname === '/'} />
+                    <NavLink href="/history" label="History" active={pathname === '/history'} />
+                    {process.env.NODE_ENV === 'development' && (
+                        <NavLink href="/settings" label="Settings" active={pathname === '/settings'} />
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 function NavLink({ href, label, active }) {
     return (
-    <Link
-    href={href}
-    className={`transition-colors hover:text-violet-500 ${
-    active ? 'text-blue-300 underline underline-offset-4' : ''
-        }`}
-    >
-        {label}
-    </Link>
+        <Link
+            href={href}
+            className={`transition-colors hover:text-violet-500 ${active ? 'text-blue-300 underline underline-offset-4' : ''
+                }`}
+        >
+            {label}
+        </Link>
     );
 }
