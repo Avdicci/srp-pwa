980c32460576535e180e394eae9c1dbcbb7c215c
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-05-12 10:53:20 +0200

    025: Change settings name to Admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,10 +2,10 @@
 import DevSeedButton from '../../components/DevSeedButton'
 import ClearHistoryButton from '../../components/ClearHistoryButton'
 
-export default function SettingsPage() {
+export default function AdminPage() {
     return (
         <div className="p-6 max-w-4xl mx-auto">
-            <h1 className="text-3xl font-bold mb-6 text-center">Settings</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center">Admin</h1>
 
             <div className="space-y-6">
                 {/* Only show this in development */}
