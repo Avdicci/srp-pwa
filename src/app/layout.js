39118d0e3c746da244c54f2fc0a70a960c511d8c
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-05-19 15:02:51 +0200

    017-add PWA support

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import RegisterSW from './register-sw';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,19 +13,20 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "SRP PWA",
-  description: "A simple SRP-based Progressive Web App",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="theme-color" content="#FC6B03" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background-section text-black`}
       >
         <Navbar />
         <main className="p-4">{children}</main>
+        <RegisterSW />
       </body>
     </html>
   );
